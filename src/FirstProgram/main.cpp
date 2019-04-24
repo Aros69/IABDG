feb026c2bb646c7af98c8ba5800bef3a75191865
Robin Donnay <robin.donnay@etu.univ-lyon1.fr>
2019-04-24 15:59:54 +0200

    Add Background Elimination class

--- src/FirstProgram/main.cpp
+++ src/FirstProgram/main.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 
-#include "Webcam.h"
+#include "BackgroundFiltering.h"
 
 int main() {
     std::cout << "Hello World !\n";
 
-    const char *WIN_RF = "TestWindow";
-    cv::namedWindow(WIN_RF, cv::WINDOW_AUTOSIZE);
-    cv::moveWindow(WIN_RF, 400, 0);
-    cv::Ptr<cv::BackgroundSubtractor> pBackSub = cv::createBackgroundSubtractorKNN();
-    cv::Mat otherFrame, newFrame;
-
     Webcam& webcam = Webcam::getInstance();
+    BackgroundFiltering filtering;
 
     int delay = 10;
-    for (;;) //Show the image captured in the window and repeat
+    for (;;)
     {
         webcam.update();
-        pBackSub->apply(webcam.getActualFrame(), otherFrame, 0);
-
-        //pBackSub->getBackgroundImage(newFrame);
-
-        imshow(WIN_RF, otherFrame);
+        filtering.update();
 
         char c = (char) cv::waitKey(delay);
         if (c == 27) break;
     }
 
-    cv::destroyWindow(WIN_RF);
     std::cout << "\nBye World !\n";
     return 0;
 }
